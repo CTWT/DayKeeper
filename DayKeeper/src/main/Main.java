49aa617530c5fd25d814c8681ff7e09f71a31779
setia <inchul9603@naver.com>
2025-05-14 11:58:23 +0900

    feat : Main구성
    Main소스 생성 -> 로그인화면을 이용하여 전체 프레임 구성을 하도록 구현
    
    신인철 | 25.05.14

--- DayKeeper/src/main/Main.java
+++ DayKeeper/src/main/Main.java
@@ -1,8 +1,20 @@
 package main;
 
+import login.Login;
+
+/*
+ * 생성자 : 신인철
+ * 생성일 : 25.05.14
+ * 파일명 : Main.java
+ * 수정자 : 
+ * 수정일 :
+ * 설명 : 에플리케이션 실행
+ */
+
 public class Main {
 
     public static void main(String[] args) {
 
+        new Login().setVisible(true);
     }
 }
