b7901d60b904f333b22c4262e75b254de4818d89
setia <inchul9603@naver.com>
2025-05-15 12:01:26 +0900

    feat : 화면 전환 기능추가
    cardLayout으로 화면전환 추가, enum에 본인이 추가할 화면을 추가하면 자동으로 등록되어변경
    
    신인철 | 25.05.15

--- DayKeeper/src/main/Main.java
+++ DayKeeper/src/main/Main.java
@@ -1,11 +1,11 @@
 package main;
 
-import login.Login;
+import config.BaseFrame;
 
 /*
  * 생성자 : 신인철
  * 생성일 : 25.05.14
- * 파일명 : Main.java
+ * 파일명 : Main.jav
  * 수정자 : 
  * 수정일 :
  * 설명 : 에플리케이션 실행
@@ -13,8 +13,13 @@ import login.Login;
 
 public class Main {
 
+    /*
+     * 프로젝트 조건:
+     * - BaseFrame은 CardLayout 기반으로 화면을 전환함
+     * - Main.java에서 실행 시 첫 화면으로 로그인(Login) 화면이 보여져야 함
+     * - Login은 JPanel을 상속하며, BaseFrame.showScreen()으로 삽입됨
+     */
     public static void main(String[] args) {
-
-        new Login().setVisible(true);
+        new BaseFrame();
     }
 }
